6cc6c68aa48b7ae2f6bf934ab7c54df28ef19e54
Przemek Dorski <przemek.dorski@o2.pl>
2021-10-30 03:00:37 +0200

    Add docstring

--- zad_4-2_palindrom.py
+++ zad_4-2_palindrom.py
@@ -2,6 +2,14 @@ print("Hej, chcesz wiedzieć czy dane słowo jest palindromem?")
 
 def palindrome():
 
+    """
+    Palindrome() checks if a word is palindrome or not
+    No defined parameter, external argument = input()
+    Sollution: compare lists for argument ([normal] vs [reversed])
+    Func result: returns after bool()
+
+    """
+
     word = input("No to wpisz dowolne słowo ;): ")
 
     if list(word.lower()) == list(reversed(word.lower())):
